961bf45d0ed1357da3448f3851259299049ac428
Aaron Unger <aaron.unger@gmail.com>
2013-05-23 10:22:33 -0700

    Initial empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Labbook::Application.config.secret_key_base = '40e9040857114961122014ccd6d2fd8b3d32e3e9c40d3d798276e905fe787d7dbd1bc6c6220bcd5fe0033bf43e1bb6b6fa62cc03479b81574a371dbbcfd6a359'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Labbook::Application.config.session_store :cookie_store, key: '_labbook_session'
