7135308c9e8e748e3cb7d7497acea60b3e061031
Aaron Unger <aaron.unger@gmail.com>
2013-05-30 15:34:40 -0700

    Better alt and title text for snapshot images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def snapshot_image_tag(snapshot, size=:medium)
+    url = snapshot.content.url(size)
+    alt = snapshot.content.original_filename || "missing snapshot"
+    image_tag(url, alt: alt, title: alt)
+  end
 end
