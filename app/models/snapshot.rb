baea8c44a768b529736616a05478aa987e0d9ee7
Aaron Unger <aaron.unger@gmail.com>
2013-08-08 14:23:09 -0700

    Allow creating snapshots via url

--- app/models/snapshot.rb
+++ app/models/snapshot.rb
@@ -1,7 +1,13 @@
 class Snapshot < ActiveRecord::Base
+  attr_reader :content_remote_url
   belongs_to :album
 
   has_attached_file :content,
     styles: { medium: "500x500>", thumb: "100x100>" },
     default_url: "/images/:style/missing.png"
+
+  def content_remote_url=(url_str)
+    self.content = URI.parse(url_str)
+    @content_remote_url = url_str
+  end
 end
