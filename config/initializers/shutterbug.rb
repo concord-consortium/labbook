2651f7705dabae0918114330b05aeec0869ad26c
Aaron Unger <aaron.unger@gmail.com>
2014-12-02 09:26:49 -0800

    Use a local shutterbug js as the default

--- config/initializers/shutterbug.rb
+++ config/initializers/shutterbug.rb
@@ -1,5 +1,5 @@
 if ENV['SHUTTERBUG_JS']
   SHUTTERBUG_JS = ENV['SHUTTERBUG_JS']
 else
-  SHUTTERBUG_JS = '//shutterbug.herokuapp.com/shutterbug/shutterbug.js'
+  SHUTTERBUG_JS = '/javascripts/shutterbug.js'
 end
